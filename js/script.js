4ad089457c42e83f761a83db2b427fa547bc0abe
Lucas <lucasb.alexis@gmail.com>
2026-09-18 18:49:50 -0400

    Initial PrevaSense site

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var toggle = document.querySelector(".nav-toggle");
+  var navList = document.getElementById("nav-list");
+
+  if (toggle && navList) {
+    toggle.addEventListener("click", function () {
+      var isOpen = navList.classList.toggle("open");
+      toggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
+    });
+
+    navList.querySelectorAll("a").forEach(function (link) {
+      link.addEventListener("click", function () {
+        navList.classList.remove("open");
+        toggle.setAttribute("aria-expanded", "false");
+      });
+    });
+  }
+});
